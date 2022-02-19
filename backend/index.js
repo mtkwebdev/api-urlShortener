a6f83bb15ec0f75a9a9e714a0ec0f8c22a32078f
mtkwebdev <mtkdev1.0@gmail.com>
2022-02-19 21:15:45 +0000

    Connecting to MongoDB Atlas

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 
+const connectDB = require('./config/db')
+connectDB()
+
 const defaultPort = process.env.BASE || 5000
 
 app.listen(()=>{
