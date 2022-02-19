2614f919720d41d571bd0bf06e305e49980e4d35
mtkwebdev <mtkdev1.0@gmail.com>
2022-02-19 21:14:38 +0000

    Set up basic express Server in Node.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,10 @@
-console.log("hello")
+require('dotenv').config()
+const express = require('express');
+const app = express();
+
+const defaultPort = process.env.BASE || 5000
+
+app.listen(()=>{
+    console.log(`Server is running on ${defaultPort}`)
+})
+
