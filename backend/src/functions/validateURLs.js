20f4a2707635228831d9612ba557bdb045dd669b
mtkwebdev <mtkdev1.0@gmail.com>
2022-02-20 11:38:10 +0000

    created URL Regex Validator function

---
+++ backend/src/functions/validateURLs.js
@@ -0,0 +1,7 @@
+function validateUrl(value) {
+    return /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/i.test(
+      value
+    );
+  }
+  
+  module.exports = { validateUrl };
